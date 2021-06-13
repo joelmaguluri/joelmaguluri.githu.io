20f36a7c803a334042b992c8ae4575d7ca00a3bf
Joel Maguluri <joel.maguluri@cartedo.com>
2021-06-13 14:51:04 +0530

    tailwind configuration issue sorted

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import Navbar from "../src/components/UI/Navbar"
-import MainArea from "../src/components/Home/MainArea"
-export default function Home() {
+import Navbar from "../src/components/UI/Navbar";
+import MainArea from "../src/components/Home/MainArea";
+import { FunctionComponent } from "react";
+
+export default function Home(): FunctionComponent {
   return (
-    <>
+    <div>
       <Navbar />
       <MainArea />
-
-    </>
-  )
+    </div>
+  );
 }
--- src/components/Home/MainArea.tsx
+++ src/components/Home/MainArea.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Image from 'next/image'
-export default function MainArea() {
-    return (
-        <main>
-            <img src={'/images/homepage-cover.jpg'} className="absolute h-full w-full object-cover overflow-hidden" alt="homepagecover"/>
-            <section className="relative min-h-screen flex justify-center pt-12 lg:pt-64 px-8">
-                <h1 className="text-white font-bold cursive home-name leading-none lg:leading-snug">
-                    Hola. I am Sudeep Joel.
-                </h1>
-            </section>
-        </main>
-    )
+import React, { FunctionComponentElement } from "react";
+
+export default function MainArea(): FunctionComponentElement<any> {
+  return (
+    <main>
+      <img
+        src={"/images/homepage-cover.jpg"}
+        className="absolute h-full w-full object-cover overflow-hidden"
+        alt="Home Page Cover"
+      />
+      <section className="relative min-h-screen flex justify-center pt-12 lg:pt-64 px-8">
+        <h1 className="text-white font-bold cursive home-name leading-none lg:leading-snug">
+          Hola. I am Sudeep Joel.
+        </h1>
+      </section>
+    </main>
+  );
 }
